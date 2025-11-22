64189f6dbf1fa754e763393e0ea819de0f486254
João Eduardo Ferreira Bertacchi <joaobertacchi@gmail.com>
2025-11-22 18:32:50 -0300

    style: fix prettier styling problems

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ['@react-native'],
+  extends: ['@react-native', 'plugin:prettier/recommended'],
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error',
